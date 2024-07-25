16d4970c91d10949ae2b47a2369f5fcd54296bed
Stonino82 <antoninolattene@gmail.com>
2024-07-25 20:31:08 +0200

    update buttons

--- template-parts/navigation.php
+++ template-parts/navigation.php
@@ -14,11 +14,11 @@
     <?php wp_nav_menu( array( 
             'menu' => 'primary',
             'container_class' => 'menu-container',
-            'link_class' => 'btn btn__secondary'
+            'link_class' => 'btn btn-tertiary'
         ) );
     ?>
 
-    <a class="btn btn__primary" href="mailto:antoninolattene@gmail.com" role="btn" rel="noopener noreferrer">
+    <a class="btn btn-primary" href="mailto:antoninolattene@gmail.com" role="btn" rel="noopener noreferrer">
         <span class="btn-text">Email Me</span>
         <span class="btn-icon  btn-icon--right">
             <i class="fas fa-arrow-down rotate"></i>
